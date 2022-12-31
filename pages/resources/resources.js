eb9c4aa0b623b1bde05a9a68251b79725132af67
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-30 21:56:38 -0500

    Initial Commit

---
+++ pages/resources/resources.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Resources() {
+    return (
+        <div>
+            <h1>Welcome to Resources!</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
